6e89949c588739e2eda938da4ca28a3ba997cf7d
Jon Gallagher <jon@nubedehelado.com>
2017-01-10 13:47:43 -0800

    First version

---
+++ main.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+import json
+import urllib
+import ConfigParser
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = "ndh-secrets"
+    key = "lambda/mysql.cfg"
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+
